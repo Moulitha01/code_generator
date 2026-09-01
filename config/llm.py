1ff4dc974ea5d0215209515da8f2c6b492ab1c19
Moulitha01 <moulithac@gmail.com>
2026-09-01 06:11:42 +0530

    ollama

--- config/llm.py
+++ config/llm.py
@@ -1,14 +1,12 @@
-# llm.py
-
+import os
 from langchain_ollama import ChatOllama
 
 def get_llm(temperature=0.4):
-    """
-    Returns an Ollama LLM instance.
-    Make sure Ollama is running on localhost:11434
-    """
     return ChatOllama(
-        model="qwen2.5-coder:7b",  # must match ollama list exactly
-        base_url="http://localhost:11434",
-        temperature=0.7
+        model="qwen2.5-coder:7b",
+        base_url=os.getenv(
+            "OLLAMA_BASE_URL",
+            "http://localhost:11434"
+        ),
+        temperature=temperature
     )
